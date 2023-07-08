964591df91d19c8dfbbe62d730ba4eed5b3deb17
Aditya Kumar Singh <adityakumarsingh909@outlook.com>
2023-07-08 23:44:21 +0530

    Find the digits in a numeber.

---
+++ FindDigitsInNumber.java
@@ -0,0 +1,17 @@
+java.util.*
+class FindDigitsInNo{
+    public static void main(String[] args){
+       Scanner scan=new Scanner(System.in);
+
+       System.out.println("Enter a Number");
+       int n=scan.nextInt();
+
+       int r;
+       while(n>0) {
+         r=n%10;
+         n=n/10;
+
+         System.out.println(r);
+       }
+       System.out.println(n);
+    }
