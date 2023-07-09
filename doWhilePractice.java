ec4a18bfaf6e350f6691957f44d1532e25562940
Aditya Kumar Singh <adityakumarsingh909@outlook.com>
2023-07-09 23:47:07 +0530

    Do while loop practice.

---
+++ doWhilePractice.java
@@ -0,0 +1,10 @@
+import java.util.*;
+class switchPractic{
+  public static void main(String args[]){
+    Scanner sc=new Scanner(System.in);
+    int age=sc.nextInt();
+    do{
+      System.out.println("can drive");
+    }while(age=>18);
+  }
+}
