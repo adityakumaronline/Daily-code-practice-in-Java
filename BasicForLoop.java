4faee2a636eff37759f5fb63f70c21d1ff528113
Aditya Kumar Singh <adityakumarsingh909@outlook.com>
2023-07-13 22:16:21 +0530

    Update BasicForLoop.java

--- BasicForLoop.java
+++ BasicForLoop.java
@@ -1,7 +1,7 @@
 class ForLoop{
   public static void main(String args[]){
-    for(int i=1;i<=11;i++){
-      System.out.println("Aditya");
+    for(int i=1;i<=10;i++){
+      System.out.println("Hello World");
     }
   }
 }
